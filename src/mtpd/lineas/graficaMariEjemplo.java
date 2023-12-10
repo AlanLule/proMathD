9ea361893ad0653d45d6732ff9c07e98f600b996
Rigel <Rigel@DESKTOP-3G27VH5>
2023-12-09 23:49:44 -0600

    Carga Inicial

---
+++ src/mtpd/lineas/graficaMariEjemplo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mtpd.lineas;
+
+/**
+ *
+ * @author Rigel Lule
+ */
+public class graficaMariEjemplo {
+ private float [] time;
+ private float [] fp;
+ private float N = 100;
+ 
+ public graficaMariEjemplo(){
+     time = new float[N];
+     fp = new float [N]:
+ }
+ 
+ private void compute (){
+     
+ }
+ 
+}
